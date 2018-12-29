c2de90296633ecba2be5e55a634b84c9bce3e176
Ahmed Belal <ahmedbelalnour@gmail.com>
2018-12-29 10:50:53 +0000

    adding PID source code from my windows environment repo, you can find it on my git repos here named (CarND-PID-Control-Project)

--- src/PID.cpp
+++ src/PID.cpp
@@ -10,12 +10,29 @@ PID::PID() {}
 
 PID::~PID() {}
 
-void PID::Init(double Kp, double Ki, double Kd) {
+void PID::Init(double Kp, double Ki, double Kd) 
+{
+	this->Kp = Kp;
+	this->Ki = Ki;
+	this->Kd = Kd;
+
+	//Initializing proportional, integral, derivative errors to zero.
+	this->p_error = 0.0;
+	this->i_error = 0.0;
+	this->d_error = 0.0;
 }
 
-void PID::UpdateError(double cte) {
+void PID::UpdateError(double cte) 
+{
+	// derivative error: is the difference between the old cte (p_error) and the new cte
+	d_error = cte - p_error;
+	// proportional error: is the cte value
+	p_error = cte;
+	// integral error: is the sum of all cte values
+	i_error += cte;
 }
 
-double PID::TotalError() {
+double PID::TotalError() 
+{
+	return (-Kp * p_error - Ki * i_error - Kd * d_error);
 }
-
